a773ee4b7babde8f641316fce750238f195b610b
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-18 22:49:13 +0300

    started

--- src/components/GraphEditor/GraphEditor.vue
+++ src/components/GraphEditor/GraphEditor.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import { type CellStyle, Graph, InternalEvent } from '@maxgraph/core';
+import { onMounted } from 'vue';
+onMounted(() => {
+    const container = <HTMLElement>document.getElementById('graph-container');
+    // Disables the built-in context menu
+    InternalEvent.disableContextMenu(container);
+    const graph = new Graph(container);
+    graph.setPanning(true); // Use mouse right button for panning
+    // // Gets the default parent for inserting new cells. This
+    // // is normally the first child of the root (ie. layer 0).
+    const parent = graph.getDefaultParent();
 
-const container = <HTMLElement>document.getElementById('graph-container');
-// Disables the built-in context menu
-InternalEvent.disableContextMenu(container);
-
-const graph = new Graph(container);
-graph.setPanning(true); // Use mouse right button for panning
-// Gets the default parent for inserting new cells. This
-// is normally the first child of the root (ie. layer 0).
-const parent = graph.getDefaultParent();
-
-// Adds cells to the model in a single step
-graph.batchUpdate(() => {
-    const vertex01 = graph.insertVertex(parent, null, 'a regular rectangle', 10, 10, 100, 100);
-    const vertex02 = graph.insertVertex(parent, null, 'a regular ellipse', 350, 90, 50, 50, <CellStyle>{ shape: 'ellipse', fillColor: 'orange' });
-    graph.insertEdge(parent, null, 'a regular edge', vertex01, vertex02);
+    // Adds cells to the model in a single step
+    graph.batchUpdate(() => {
+        const vertex01 = graph.insertVertex(parent, null, 'a regular rectangle', 10, 10, 100, 100);
+        const vertex02 = graph.insertVertex(parent, null, 'a regular ellipse', 350, 90, 50, 50, <CellStyle>{ shape: 'ellipse', fillColor: 'orange' });
+        graph.insertEdge(parent, null, 'a regular edge', vertex01, vertex02);
+        console.log(vertex01)
+    })
 
 })
 </script>
 
 <template>
+    <h1>This is the Graph Editor. You can design your own strategy here.</h1>
+
     <div id="graph-container"></div>
 </template>
--- src/views/GraphEditorView.vue
+++ src/views/GraphEditorView.vue
@@ -3,6 +3,5 @@ import GraphEditor from '@/components/GraphEditor/GraphEditor.vue';
 </script>
 
 <template>
-    <h1>This is the Graph Editor. You can design your own strategy here.</h1>
     <GraphEditor></GraphEditor>
 </template>
