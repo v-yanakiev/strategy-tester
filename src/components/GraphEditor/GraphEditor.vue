e0ac1df14ae9c8343344f250fdcb524c910b4007
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-21 23:33:18 +0300

    progress

--- src/components/GraphEditor/GraphEditor.vue
+++ src/components/GraphEditor/GraphEditor.vue
@@ -14,25 +14,65 @@ onMounted(() => {
 	const container = <HTMLElement>document.getElementById('graph-container');
 	// Disables the built-in context menu
 	InternalEvent.disableContextMenu(container);
-	graph = new Graph(container, undefined,);
+	graph = new Graph(container, undefined);
 	graph.setPanning(true); // Use mouse right button for panning
 	// Gets the default getParent() for inserting new cells. This
 	// is normally the first child of the root (ie. layer 0).
 	attachDeleteFunctionality();
 	// Adds cells to the model in a single step
 	graph.batchUpdate(() => {
-		beginningVertex = graph.insertVertex(getParent(), null, 'Start', 10, 10, 100, 100, <CellStyle>{ deletable: true, fillColor: 'pink' });
-		endVertex = graph.insertVertex(getParent(), null, 'End', 400, 400, 100, 100, <CellStyle>{ deletable: true, fillColor: 'pink' });
-	})
-
-})
+		beginningVertex = graph.insertVertex(
+			getParent(),
+			null,
+			'Start',
+			10,
+			10,
+			100,
+			100,
+			<CellStyle>{ deletable: true, fillColor: 'pink' }
+		);
+		endVertex = graph.insertVertex(
+			getParent(),
+			null,
+			'End',
+			400,
+			400,
+			100,
+			100,
+			<CellStyle>{ deletable: true, fillColor: 'pink' }
+		);
+	});
+});
 function addIfBlock(statement: string) {
-	const ifVertex = graph.insertVertex(getParent(), null, `If: ${statement}`, 200, 200, 100, 100, <CellStyle>{ deletable: true, fillColor: 'orange' });
+	const ifVertex = graph.insertVertex(
+		getParent(),
+		null,
+		`If: ${statement}`,
+		200,
+		200,
+		100,
+		100,
+		<CellStyle>{ deletable: true, fillColor: 'orange' }
+	);
 }
-function addWhileBlock(statement: string) {
-}
+function addWhileBlock(statement: string) { }
 function addCodeExecution(statement: string) {
-	const codeExecution = graph.insertVertex(getParent(), null, `Execute code: ${statement}`, 250, 250, 100, 100, <CellStyle>{ deletable: true, fillColor: 'lightblue', whiteSpace: "wrap", overflow: "width", autosize: true });
+	const codeExecution = graph.insertVertex(
+		getParent(),
+		null,
+		`Execute code: ${statement}`,
+		250,
+		250,
+		100,
+		100,
+		<CellStyle>{
+			deletable: true,
+			fillColor: 'lightblue',
+			whiteSpace: 'wrap',
+			overflow: 'width',
+			autosize: true
+		}
+	);
 }
 function attachDeleteFunctionality() {
 	document.onkeydown = function (evt) {
@@ -41,10 +81,9 @@ function attachDeleteFunctionality() {
 			// Get the currently selected cells
 			var selectedCells = graph.getSelectionCells();
 			graph.removeCells(selectedCells);
-			selectedCells.forEach(cell => graph.refresh(cell));
+			selectedCells.forEach((cell) => graph.refresh(cell));
 		}
 	};
-
 }
 </script>
 
@@ -58,12 +97,11 @@ function attachDeleteFunctionality() {
 		<AddCodeExecution @finalized="addCodeExecution" />
 	</div>
 	<br />
-	<div id="graph-container" style="background-color:white;"></div>
+	<div id="graph-container" style="background-color: white"></div>
 </template>
 <style scoped>
 .elementCreators {
 	display: flex;
 	flex-direction: column;
-	;
 }
 </style>
