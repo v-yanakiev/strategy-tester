d6bc9bb232734103f85994f1f3abb1ae20188e6b
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-22 21:48:55 +0300

    progress

--- src/components/GraphEditor/AddNewElement/AddPath.vue
+++ src/components/GraphEditor/AddNewElement/AddPath.vue
@@ -10,22 +10,53 @@ let endNode: Ref<Cell | undefined> = ref(undefined);
 
 const props = defineProps<Props>();
 const emits = defineEmits(['finalized']);
+enum PathChosen {
+    Yes, No
+}
+const pathChosen = ref(undefined) as Ref<undefined | PathChosen>;
+
 function finalize() {
     emits("finalized", beginningNode.value, endNode.value)
     beginningNode.value = undefined;
     endNode.value = undefined;
+    pathChosen.value = undefined;
 }
-function markPathBeginning() {
+function markPathBeginning(selectedPathChosen?: PathChosen) {
     beginningNode.value = props.markedNode;
+    pathChosen.value = selectedPathChosen;
 }
 function markPathEnd() {
     endNode.value = props.markedNode;
     finalize();
 }
+function nodeIsConditionalNode() {
+    return (props.markedNode?.value as string | undefined)?.startsWith("If");
+}
 </script>
 <template>
-    <button @click="markPathEnd" v-if="beginningNode" :disabled="!markedNode || markedNode == beginningNode">
-        End path
-    </button>
-    <button @click="markPathBeginning" v-else :disabled="!markedNode">Start path</button>
+    <div v-if="nodeIsConditionalNode()">
+
+        <button @click="markPathEnd" v-if="beginningNode && pathChosen == PathChosen.Yes"
+            :disabled="!markedNode || markedNode == beginningNode">
+            End Yes path
+        </button>
+        <button @click="markPathBeginning(PathChosen.Yes)" v-else-if="!pathChosen" :disabled="!markedNode">
+            Start Yes path
+        </button>
+
+        <button @click="markPathEnd" v-if="beginningNode && pathChosen == PathChosen.No" :disabled="!markedNode ||
+            markedNode == beginningNode">
+            End No path
+        </button>
+        <button @click="markPathBeginning(PathChosen.No)" v-else-if="!pathChosen" :disabled="!markedNode">
+            Start No path
+        </button>
+
+    </div>
+    <div v-else>
+        <button @click="markPathEnd" v-if="beginningNode" :disabled="!markedNode || markedNode == beginningNode">
+            End path
+        </button>
+        <button @click="markPathBeginning()" v-else :disabled="!markedNode">Start path</button>
+    </div>
 </template>
