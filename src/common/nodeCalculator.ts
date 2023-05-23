b1025974ab2f7f218fe0721468d33b039049fe5e
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-23 21:48:12 +0300

    maybe

--- src/common/nodeCalculator.ts
+++ src/common/nodeCalculator.ts
@@ -1,10 +1,7 @@
 import type { Cell } from '@maxgraph/core';
 
 export function isCondition(node: Cell) {
-    return (
-        (node.value as string).startsWith('If') ||
-        (node.value as string).startsWith('While')
-    );
+    return (node.value as string).startsWith('If');
 }
 export function isEnd(node: Cell) {
     return node.value == 'End';
@@ -14,4 +11,10 @@ export function isStart(node: Cell) {
 }
 export function leadsNowhere(node?: Cell) {
     return !node?.getOutgoingEdges().length;
+}
+export function getCondition(node: Cell) {
+    return (node.value as string).slice('If: '.length)[0];
+}
+export function getCodeToExecute(node: Cell) {
+    return (node.value as string).slice('Execute code: '.length)[0];
 }
