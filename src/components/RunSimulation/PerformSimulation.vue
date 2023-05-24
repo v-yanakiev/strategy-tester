10175af741e7bd645248315641299d2295cc247b
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-24 21:51:12 +0300

    fix

--- src/components/RunSimulation/PerformSimulation.vue
+++ src/components/RunSimulation/PerformSimulation.vue
@@ -3,11 +3,16 @@ import { useGraphStore } from '@/stores/graphStore';
 import { useParsedDataStore } from '@/stores/parsedDataStore';
 import { ref } from 'vue';
 import CanSimulationBeRan from '../common/CanSimulationBeRan.vue';
-const simulationCanBeRan = ref(false);
-function setCalculationResult(resultValue: boolean) {
-    simulationCanBeRan.value = resultValue;
+
+const simulationIsRunning = ref(false);
+function runSimulation() {
+    simulationIsRunning.value = true;
 }
+function stopSimulation() {}
 </script>
 <template>
-    <button>Run simulation</button>
+    <button v-if="simulationIsRunning" @click="runSimulation">
+        Run simulation
+    </button>
+    <button @click="stopSimulation" v-else>Stop simulation</button>
 </template>
--- src/stores/simulationStore.ts
+++ src/stores/simulationStore.ts
@@ -1,12 +1,24 @@
 import { defineStore } from 'pinia';
 import { ref, type Ref } from 'vue';
 export const useSimulationStore = defineStore('simulation', () => {
-    const dependentVariableName = ref(null) as Ref<any>;
+    const dependentVariableName = ref(null) as Ref<null | string>;
+    const dependentVariableInitialValue = ref(null) as Ref<any>;
     function setDependentVariableName(value: string) {
         dependentVariableName.value = value;
     }
     function getDependentVariableName() {
         return dependentVariableName.value;
     }
-    return { setDependentVariableName, getDependentVariableName };
+    function setDependentVariableInitialValue(value: any) {
+        dependentVariableInitialValue.value = value;
+    }
+    function getDependentVariableInitialValue() {
+        return dependentVariableInitialValue.value;
+    }
+    return {
+        setDependentVariableName,
+        getDependentVariableName,
+        setDependentVariableInitialValue,
+        getDependentVariableInitialValue
+    };
 });
