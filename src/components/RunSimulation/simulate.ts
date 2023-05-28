38b1eca6a339c193a492fd5301c54bea11b17c92
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-28 17:17:50 +0300

    new

--- src/components/RunSimulation/simulate.ts
+++ src/components/RunSimulation/simulate.ts
@@ -43,7 +43,7 @@ export async function simulate() {
         } else {
             //can be precalculated across all steps
             nodeAndItsConditionsResultOverTime.set(condition.id!, []);
-            for (let i = 0; i < 5000; i++) {
+            for (let i = 0; i < steps.length; i++) {
                 calculate(
                     poolToUse,
                     condition,
