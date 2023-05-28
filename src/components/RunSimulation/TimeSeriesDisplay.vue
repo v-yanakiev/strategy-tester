fd24ff506ad262ec01d9fcec1fb5f8b9f158eb68
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-28 19:33:40 +0300

    fix

---
+++ src/components/RunSimulation/TimeSeriesDisplay.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { simulate } from './simulate';
+import { SimulationState, useSimulationStore } from '@/stores/simulationStore';
+
+const simulationStore = useSimulationStore();
+function runSimulation() {
+    simulate();
+}
+</script>
+<template></template>
