40e1768eb706efeef250095cb8899fe4b5196917
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-05-29 23:21:14 +0300

    fix

--- src/components/RunSimulation/TimeSeriesDisplay.vue
+++ src/components/RunSimulation/TimeSeriesDisplay.vue
@@ -5,21 +5,29 @@ import { useSimulationStore } from '@/stores/simulationStore';
 import { useParsedDataStore } from '@/stores/parsedDataStore';
 const simulationStore = useSimulationStore();
 const parsedDataStore = useParsedDataStore();
+const originalData = parsedDataStore.getNonProxyParsedData()!;
+const priceVariableName = parsedDataStore.priceVariableName!;
+const timeVariableName = parsedDataStore.timeVariableName!;
+
 onMounted(async () => {
-    const originalData = parsedDataStore.getNonProxyParsedData()!;
+    const dataToSend = originalData.data
+        .map((step, index) => {
+            let parsed = Date.parse(step[timeVariableName]);
+            const date = new Date(parsed);
+            const price = step[priceVariableName];
+            if (isNaN(date.getTime()) || !(typeof price == 'number')) {
+                return null;
+            }
+            return [date, price];
+        })
+        .filter((a) => a) as [Date, number][];
     setTimeout(() => {
         const graph = new Dygraph(
             document.getElementById('graphDiv')!,
-            originalData.data.map((step) => {
-                const toReturn = [
-                    new Date(step[parsedDataStore.timeVariableName!]),
-                    step[parsedDataStore.priceVariableName!]
-                ];
-                return toReturn;
-            }),
-            { labels: ['Date', 'Price'] }
+            dataToSend,
+            { labels: ['Date', 'Price'], connectSeparatedPoints: false }
         );
-    }, 100);
+    }, 1000);
 });
 </script>
 <template>
