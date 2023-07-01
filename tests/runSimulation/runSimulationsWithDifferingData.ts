6768757c08097a2754291fd62e84c0885359244f
Vasil Yanakiev <vasil.yanakiev@docuware.com>
2023-07-01 22:38:20 +0300

    new

--- tests/runSimulation/runSimulationsWithDifferingData.ts
+++ tests/runSimulation/runSimulationsWithDifferingData.ts
@@ -1,11 +1,41 @@
 import { NightwatchBrowser } from 'nightwatch';
-import { inputOtherData } from '../csvInput/inputOtherDataAndSelectAllRequired';
 import { checkForBrowserExceptions } from '../errorChecking/checkForBrowserExceptions';
 import * as strategySelection from '../graphEditor/selectExampleStrategies';
 import { createCustomStrategy } from '../graphEditor/createCustomStrategy';
 async function navigateToRunSimulation(browser: NightwatchBrowser) {
     await browser.pause(500);
     await browser.click('#runSimulationLink');
+}
+const path = require('path');
+
+const AAPLEarlyDataFilePath = path.resolve(
+    __dirname,
+    '../../data/AAPLEarlyData.csv'
+);
+
+async function inputOtherData(browser: NightwatchBrowser) {
+    await browser.click('#dataInputLink');
+    await browser.pause(200);
+    await browser.execute(function () {
+        (document!.querySelector('#fileInput') as HTMLElement)!.style!.display =
+            'block';
+    });
+    await browser.setValue('#fileInput', AAPLEarlyDataFilePath);
+    await browser.execute(function () {
+        (document!.querySelector('#fileInput') as HTMLElement)!.style!.display =
+            'none';
+    });
+    await checkForBrowserExceptions(browser);
+    await browser.waitForElementVisible('#initialBalanceInput', 10000);
+    await browser.setValue('#initialBalanceInput', '10000');
+    await browser.click('#initialBalanceConfirmation');
+    await browser.pause(500);
+    // Test time variable selection
+    await browser.click(`#possibleTimeVariables input[value="Date"]`);
+    await browser.pause(500);
+    // Test price variable selection
+    await browser.click(`#possiblePriceVariables input[value="Open"]`);
+    await browser.pause(500);
 }
 
 async function runSimulation(browser: NightwatchBrowser) {
